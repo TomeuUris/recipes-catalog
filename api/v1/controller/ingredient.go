03676904a265a33124c51d035a9b6ac8018eb228
Tomeu Uris <tomeu.uris.dev@gmail.com>
2023-12-17 16:17:18 +0100

    Add ingredient controller and routes

---
+++ api/v1/controller/ingredient.go
@@ -0,0 +1,83 @@
+package controller
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/TomeuUris/recipes-catalog/api/v1/payload"
+	"github.com/TomeuUris/recipes-catalog/pkg/entity"
+	"github.com/TomeuUris/recipes-catalog/pkg/repo/ingredient"
+	"github.com/gin-gonic/gin"
+)
+
+type IngredientController struct {
+	repo ingredient.Repo
+}
+
+func NewController(repo ingredient.Repo) *IngredientController {
+	return &IngredientController{repo: repo}
+}
+
+// @Summary Get ingredient by ID
+// @Description get User by its ID
+// @Tags Ingredients
+// @Produce  json
+// @Param   id     path    int     true        "Ingredient ID"
+// @Success 200 {object} entity.Ingredient
+// @Router /ingredients/{id} [get]
+func (c *IngredientController) GetIngredientByIdHandler(ctx *gin.Context) {
+	// Get the ingredient ID from the URL parameter
+	ingredientIDStr := ctx.Param("id")
+	ingredientID, err := strconv.Atoi(ingredientIDStr)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ingredient ID"})
+		return
+	}
+
+	// Get the ingredient from the database
+	ingredients, err := c.repo.FindByID(ctx, ingredientID)
+	if err != nil {
+		if errors.Is(err, entity.ErrNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return the ingredients as a response
+	ctx.JSON(http.StatusOK, ingredients)
+}
+
+// @Summary Create ingredient
+// @Description Create a new Ingredient
+// @Accept  json
+// @Produce  json
+// @Param   ingredient     body    payload.Ingredient     true        "Ingredient info"
+// @Success 200 {object} entity.Ingredient
+// @Router /ingredients [post]
+func (c *IngredientController) CreateIngredientHandler(ctx *gin.Context) {
+	// Parse the request payload
+	var ingredientPayload payload.Ingredient
+	if err := ctx.ShouldBindJSON(&ingredientPayload); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ingredient := ingredientPayload.Entity()
+	// Logic to create the ingredient in the database
+	err := c.repo.Add(ctx, ingredient)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return the created ingredient as a response
+	ctx.JSON(http.StatusCreated, ingredient)
+}
+
+func SetupRouter(controller *IngredientController, router *gin.Engine) *gin.Engine {
+	router.GET("/ingredients/:id", controller.GetIngredientByIdHandler)
+	router.POST("/ingredients", controller.CreateIngredientHandler)
+	return router
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/TomeuUris/recipes-catalog/pkg/entity"
+	"github.com/TomeuUris/recipes-catalog/api/v1/controller"
+	_ "github.com/TomeuUris/recipes-catalog/docs"
 	"github.com/TomeuUris/recipes-catalog/pkg/repo/ingredient"
 	"github.com/TomeuUris/recipes-catalog/pkg/repo/recipe"
 	"github.com/TomeuUris/recipes-catalog/pkg/sqlite"
+	"github.com/gin-gonic/gin"
+	_ "github.com/swaggo/files"
+	_ "github.com/swaggo/gin-swagger"
 )
 
 type Repo struct {
@@ -17,54 +18,63 @@ type Repo struct {
 
 func main() {
 	db := sqlite.MustOpen("database.sqlite")
-	ctx := context.Background()
+	r := gin.Default()
+	ingredientsController := controller.NewController(ingredient.NewRepo(db))
+	r = controller.SetupRouter(ingredientsController, r)
 
-	repos := Repo{
-		Ingredients: ingredient.NewRepo(db),
-		Recipes:     recipe.NewRepo(db),
-	}
-
-	espaguetis := &entity.Ingredient{
-		Name: "spaghetti",
-		Type: "pasta",
-	}
-
-	tomate := &entity.Ingredient{
-		Name: "tomate",
-		Type: "salsa",
-	}
-
-	repos.Ingredients.Add(ctx, espaguetis)
-	fmt.Println(espaguetis.ID)
-	repos.Ingredients.Add(ctx, tomate)
-	fmt.Println(tomate.ID)
-
-	espaguetisConTomate := &entity.Recipe{
-		Name:        "spaghetti con tomate",
-		Ingredients: []*entity.Ingredient{espaguetis, tomate},
-		Steps: []string{
-			"Cocer la pasta",
-			"Preparar la salsa",
-			"Mezclar la pasta con la salsa",
-		},
-	}
-
-	repos.Recipes.Add(ctx, espaguetisConTomate)
-	fmt.Println("ID: ", espaguetisConTomate.ID)
-
-	recipe, err := repos.Recipes.FindByID(ctx, espaguetisConTomate.ID)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("ID: ", recipe.ID)
-	fmt.Println("Name: ", recipe.Name)
-	fmt.Println("Ingredients: ")
-	for _, ingredient := range recipe.Ingredients {
-		fmt.Println(" - ", ingredient.Name, ingredient.Type)
-	}
-	fmt.Println("Steps: ")
-	for i, step := range recipe.Steps {
-		fmt.Println(" ", i+1, " ", step)
-	}
+	r.Run()
 }
+
+// func test() {
+// 	db := sqlite.MustOpen("database.sqlite")
+// 	ctx := context.Background()
+
+// 	repos := Repo{
+// 		Ingredients: ingredient.NewRepo(db),
+// 		Recipes:     recipe.NewRepo(db),
+// 	}
+
+// 	espaguetis := &entity.Ingredient{
+// 		Name: "spaghetti",
+// 		Type: "pasta",
+// 	}
+
+// 	tomate := &entity.Ingredient{
+// 		Name: "tomate",
+// 		Type: "salsa",
+// 	}
+
+// 	repos.Ingredients.Add(ctx, espaguetis)
+// 	fmt.Println(espaguetis.ID)
+// 	repos.Ingredients.Add(ctx, tomate)
+// 	fmt.Println(tomate.ID)
+
+// 	espaguetisConTomate := &entity.Recipe{
+// 		Name:        "spaghetti con tomate",
+// 		Ingredients: []*entity.Ingredient{espaguetis, tomate},
+// 		Steps: []string{
+// 			"Cocer la pasta",
+// 			"Preparar la salsa",
+// 			"Mezclar la pasta con la salsa",
+// 		},
+// 	}
+
+// 	repos.Recipes.Add(ctx, espaguetisConTomate)
+// 	fmt.Println("ID: ", espaguetisConTomate.ID)
+
+// 	recipe, err := repos.Recipes.FindByID(ctx, espaguetisConTomate.ID)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Println("ID: ", recipe.ID)
+// 	fmt.Println("Name: ", recipe.Name)
+// 	fmt.Println("Ingredients: ")
+// 	for _, ingredient := range recipe.Ingredients {
+// 		fmt.Println(" - ", ingredient.Name, ingredient.Type)
+// 	}
+// 	fmt.Println("Steps: ")
+// 	for i, step := range recipe.Steps {
+// 		fmt.Println(" ", i+1, " ", step)
+// 	}
+// }
