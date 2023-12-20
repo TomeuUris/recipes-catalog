107dd0ce441d5c006fab0d5bcb0d6d37e2e7692d
Tomeu Uris <tomeu.uris.dev@gmail.com>
2023-12-20 21:38:20 +0100

    Add Gorm implementation for Ingredient repository

---
+++ pkg/repo/ingredient/gorm.go
@@ -0,0 +1,72 @@
+package ingredient
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+
+	"github.com/TomeuUris/recipes-catalog/pkg/entity"
+)
+
+type Ingredient struct {
+	gorm.Model
+	Name string
+	Type string
+}
+
+func (i *Ingredient) ToEntity() *entity.Ingredient {
+	return &entity.Ingredient{
+		ID:   int64(i.ID),
+		Name: i.Name,
+		Type: i.Type,
+	}
+}
+
+func (i *Ingredient) FromEntity(ingredient *entity.Ingredient) {
+	i.ID = uint(ingredient.ID)
+	i.Name = ingredient.Name
+	i.Type = ingredient.Type
+}
+
+type RepoGorm struct {
+	db *gorm.DB
+}
+
+func NewGormRepo(db *gorm.DB) *RepoGorm {
+	return &RepoGorm{
+		db: db,
+	}
+}
+
+func (r *RepoGorm) FindByID(ctx context.Context, id int) (*entity.Ingredient, error) {
+	ingredient := &Ingredient{}
+	if err := r.db.WithContext(ctx).First(ingredient, id).Error; err != nil {
+		return nil, err
+	}
+
+	return ingredient.ToEntity(), nil
+}
+
+func (r *RepoGorm) FindByFilter(ctx context.Context, f *FindFilter) ([]*entity.Ingredient, error) {
+	var ingredients []*Ingredient
+	if err := r.db.WithContext(ctx).Where("recipe_id = ?", f.RecipeId).Find(&ingredients).Error; err != nil {
+		return nil, err
+	}
+	result := make([]*entity.Ingredient, len(ingredients))
+	for i, ingredient := range ingredients {
+		result[i] = ingredient.ToEntity()
+	}
+
+	return result, nil
+}
+
+func (r *RepoGorm) Add(ctx context.Context, ingredient *entity.Ingredient) error {
+	i := &Ingredient{}
+	i.FromEntity(ingredient)
+	err := r.db.WithContext(ctx).Create(i).Error
+	if err != nil {
+		return err
+	}
+	ingredient.ID = int64(i.ID)
+	return nil
+}
---
+++ pkg/repo/ingredient/sqlite.go
@@ -0,0 +1,74 @@
+package ingredient
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/TomeuUris/recipes-catalog/pkg/entity"
+)
+
+func NewRepo(db *sql.DB) *RepoSQL {
+	return &RepoSQL{
+		db: db,
+	}
+}
+
+type RepoSQL struct {
+	db *sql.DB
+}
+
+func (r *RepoSQL) FindByID(ctx context.Context, id int) (*entity.Ingredient, error) {
+	// Get ingredient from database
+	row := r.db.QueryRowContext(ctx, `SELECT id, name, type FROM ingredients WHERE id = ?`, id)
+	if err := row.Err(); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, entity.ErrNotFound
+		}
+		return nil, err
+	}
+
+	ingredient := &entity.Ingredient{}
+	row.Scan(&ingredient.ID, &ingredient.Name, &ingredient.Type)
+
+	return ingredient, nil
+}
+
+func (r *RepoSQL) FindByFilter(ctx context.Context, f *FindFilter) ([]*entity.Ingredient, error) {
+	rows, err := r.db.QueryContext(ctx, `SELECT * FROM ingredients WHERE recipeId = ?`, f.RecipeId)
+	if err != nil {
+		return nil, err
+	}
+
+	var ingredients []*entity.Ingredient
+	for rows.Next() {
+		var ingredient *entity.Ingredient
+		rows.Scan(ingredient)
+		ingredients = append(ingredients, &entity.Ingredient{
+			ID:   ingredient.ID,
+			Name: ingredient.Name,
+			Type: ingredient.Type,
+		})
+	}
+
+	return ingredients, nil
+}
+
+func (r *RepoSQL) Add(ctx context.Context, ingredient *entity.Ingredient) error {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	result, err := tx.ExecContext(ctx, `INSERT INTO ingredients (name, type) VALUES (?, ?)`, ingredient.Name, ingredient.Type)
+	if err != nil {
+		return err
+	}
+
+	ingredient.ID, err = result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
