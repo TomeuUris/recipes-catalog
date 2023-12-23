d19f63985347091c6948c773cc87ee73b84a7aea
Tomeu Uris <tomeu.uris.dev@gmail.com>
2023-12-23 14:31:26 +0100

    Add error handling functions for entity package

--- pkg/entity/errors.go
+++ pkg/entity/errors.go
@@ -7,3 +7,21 @@ var ErrNotFound = errors.New("entity not found")
 func IsErrNotFound(err error) bool {
 	return errors.Is(err, ErrNotFound)
 }
+
+var ErrInvalidEntity = errors.New("invalid entity")
+
+func IsErrInvalidEntity(err error) bool {
+	return errors.Is(err, ErrInvalidEntity)
+}
+
+var ErrInvalidID = errors.New("invalid ID")
+
+func IsErrInvalidID(err error) bool {
+	return errors.Is(err, ErrInvalidID)
+}
+
+var ErrAlreadyExists = errors.New("entity already exists")
+
+func IsErrAlreadyExists(err error) bool {
+	return errors.Is(err, ErrAlreadyExists)
+}
