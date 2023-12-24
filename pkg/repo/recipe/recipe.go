33b29256568f2b9b29022f1044a844575dc281a1
Tomeu Uris <tomeu.uris.dev@gmail.com>
2023-12-24 09:14:24 +0100

    Enabled FindByFilter and CountByFilter methods in Recipe repo interface

--- pkg/repo/recipe/recipe.go
+++ pkg/repo/recipe/recipe.go
@@ -8,14 +8,13 @@ import (
 
 type Repo interface {
 	FindByID(ctx context.Context, id int64) (*entity.Recipe, error)
-	// FindByFilter(f *FindFilter) ([]*entity.Recipe, error)
-	// CountByFilter(f *FindFilter) (int, error)
+	FindByFilter(ctx context.Context, f *FindFilter) ([]*entity.Recipe, error)
+	CountByFilter(ctx context.Context, f *FindFilter) (int, error)
 	Add(ctx context.Context, recipe *entity.Recipe) error
 	Edit(ctx context.Context, recipe *entity.Recipe) error
 	Delete(ctx context.Context, recipe *entity.Recipe) error
 }
 
-// type FindFilter struct {
-// 	RecipeIDs []int64
-// 	Name      string
-// }
+type FindFilter struct {
+	Id int `form:"id"`
+}
